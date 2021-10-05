8d65fa3557333166253d97c37ff577207c53ab26
nemo <18136225579@163.com>
2021-10-05 11:58:44 +0800

    init config

---
+++ config.ts
@@ -0,0 +1,36 @@
+export const copywriter = {
+    global: {
+
+    },
+    home: {
+        matrixIntroduction: 'An Open Virtual World for Decentralized Applications',
+        introductionSection: {
+            introduction1: {
+                title: 'Multichain Metaverse',
+                content: `Matrix is a distinctive virtual world that will support different blockchain infrastructures. Players can explore the world with different on-chain identities.`
+            },
+            introduction2: {
+                title: '3D Immersive dApp',
+                content: `Creators can enrich and extend the world by building 3D immersive decentralized applications (dApp) that connect external services and applications. For example, players can visually trade and purchase NFTs through 3D dApps from OpenSea and other marketplaces.`
+            },
+            introduction3: {
+                title: 'Programable NFTs & Objects',
+                content: `All in-game object and NFTs have their own lifecycles. Landowners can customize their behaviors, visual looking, and transformations via Turing-complete programs.`
+            },
+            introduction4: {
+                title: 'Create With Automation',
+                content: `Importing various NFTs on the blockchain and create in-game building and constructions with automation tooling.`
+            },
+            introduction5: {
+                title: 'Gameplay & Entertainment',
+                content: `Both public and user-created entertainment venues will be built in the virtual world, including 3D games, NFT galleries, and sports live centers. Players can enjoy their second life here.`
+            }
+        }
+    },
+    earlyAccess: {
+
+    },
+    faq: {
+
+    }
+}
