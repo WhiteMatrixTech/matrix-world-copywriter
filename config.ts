a31ef3699a701d9de5eab44e10e2b40c39e3b786
liyw <1539603898@qq.com>
2021-10-08 16:05:50 +0800

    update early-access text

--- config.ts
+++ config.ts
@@ -36,15 +36,15 @@ export const copywriter = {
   earlyAccess: {
     referralContest: {
       title: 'Referral Contest',
-      content: `We will open the pre-registration contest from October 3th to October 18th.
+      content: `<span class="white">We will open the pre-registration contest from October 3th to October 18th.</span>
 
-    A referral link will be generated on the early access page by completing the account registration. Players are encouraged to forward this link to others through Twitter, Facebook, or other mainstream social media.
+      A referral link will be generated on the early access page by completing the account registration. Players are encouraged to forward this link to others through Twitter, Facebook, or other mainstream social media.
 
-    Every valid referred registration will be evaluated and ranked. <span className="hightLight">The top 200 players</span> with the most referred registrations will be rewarded <span className="hightLight">as one pre-released Land plot</span> in the Matrix world.
+      Every valid referred registration will be evaluated and ranked. <span class="hightLight">The top 200 players</span> with the most referred registrations will be rewarded <span class="hightLight">as one pre-released Land plot</span> in the Matrix world.
 
-    We will open the pre-sale application and start the qualification review process simultaneously in mid-October. Further information about the Matrix world will be released and updated on the website after October 8th PDT.
+      We will open the pre-sale application and start the qualification review process simultaneously in mid-October. Further information about the Matrix world will be released and updated on the website after October 8th PDT.
 
-    Players can find the ranking status with the number of referred registrations on their status pane. The result will be announced soon after the pre-sale starts, and the rewards will be airdropped as the pre-sale is finished.`,
+      Players can find the ranking status with the number of referred registrations on their status pane. The result will be announced soon after the pre-sale starts, and the rewards will be airdropped as the pre-sale is finished.`,
     },
     NFTAirdrop: {
       title: 'NFT Airdrop',
@@ -55,49 +55,8 @@ export const copywriter = {
           title: 'Airdrop for Ethereum Users',
           content: `We will offer free NFT airdrops to the owner of the below NFT collections who participated in the early access. The owner of NFTs listed below will get one airdropped NFT for each NFT collection owned.
 
-        Note that multiple NFTs of the same collection only count once. For instance, If Sam has 3 Cryptopunks, 2 Meetbits, and 5 Rivermen, he will get 3 airdropped NFTs.`,
-          applicationList: [
-            {
-              name: 'Loot',
-              desc: 'Loot is randomized adventurer gear generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Loot in any way you want.',
-              link: 'https://opensea.io/collection/lootproject',
-            },
-            {
-              name: 'CryptoPunks',
-              desc: 'CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and became one of the inspirations for the ERC-721 standard. They have been featured in places like The New York Times, Christie’s of London, Art|Basel Miami, and The PBS NewsHour.',
-              link: 'https://opensea.io/collection/cryptopunks',
-            },
-            {
-              name: 'Decentraland',
-              desc: "Decentraland is an Ethereum blockchain-powered virtual world, developed and owned by its users, who can create, experience, and monetize content and applications. Join a growing community of virtual world inhabitants who are building the world's largest alternate reality economy on the blockchain. In this store, you can buy and sell land assets in MANA, DCL's native currency.",
-              link: 'https://opensea.io/collection/decentraland',
-            },
-            {
-              name: 'Meebits',
-              desc: 'The Meebits are 20,000 unique 3D voxel characters, created by a custom generative algorithm, then registered on the Ethereum blockchain.',
-              link: 'https://opensea.io/collection/meebits',
-            },
-            {
-              name: 'RiverMen',
-              desc: 'Rivermen is a gamified NFT collection based on blockchain technology, created by the Cthuwork&NA team using elements from the voxel version of the “Along the River During Qingming Festival”. The voxel version of “Along the River During Qingming Festival” is a 3D artwork reinterpreting this long scroll, as one of the top ten famous Chinese paintings. The RIVER refers to the "Bian River" in this ancient painting scroll. Each person in this scene has a unique identity. The Rivermen are the good neighbors and cool folks living in the "Bianriverse". They are not ancient nor modern, but aboriginal people of the "Bianriverse", which is a metaverse based on "Along the River During the Qingming Festival". They will spend some quality time together in the Bianriverse.',
-              link: 'https://opensea.io/collection/rivermen',
-            },
-            {
-              name: 'The Sandbox Land',
-              desc: 'The Sandbox is a community-driven platform where creators can monetize voxel assets and gaming experiences on the blockchain. The Sandbox metaverse comprises a map made up of 166,464 LANDS. LAND owners can host contests and events, stake SAND to earn and customize assets, monetize assets and experiences, vote in the metaverse governance, play games that you or others create, and more! Trade the collection and keep your eyes peeled for future drops.',
-              link: 'https://opensea.io/collection/sandbox',
-            },
-            {
-              name: 'Bored APe Yacht Club',
-              desc: 'The Bored Ape Yacht Club is a collection of 10,000 unique Bored Ape NFTs— unique digital collectibles living on the Ethereum blockchain. Your Bored Ape doubles as your Yacht Club membership card, and grants access to members-only benefits, the first of which is access to THE BATHROOM, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation. Visit www.BoredApeYachtClub.com for more details.',
-              link: 'https://opensea.io/collection/boredapeyachtclub',
-            },
-            {
-              name: 'CryptoVoxels',
-              desc: 'A virtual world on the Ethereum blockchain. Build, develop, and sell property on the blocks and streets of Cryptovoxels. Users own the land and assets, with ownership and trading history recorded permanently on the blockchain. Trade land NFTs right here on OpenSea, and keep track of the Cryptovoxels team on Twitter for news on the latest drops and developments.',
-              link: 'https://opensea.io/collection/cryptovoxels',
-            },
-          ],
+          Note that multiple NFTs of the same collection only count once. For instance, If Sam has 3 Cryptopunks, 2 Meetbits, and 5 Rivermen, he will get 3 airdropped NFTs.`,
+          applicationList: [],
         },
         section2: {
           title: 'Airdrop for Flow Users',
@@ -108,9 +67,9 @@ export const copywriter = {
           title: 'Airdrop for ChainIDE Users',
           content: `ChainIDE users who registered before October 18th and created a project with ChainIDE (You will have to ensure that the email address you use in Matrix registration is the same as the ChainIDE account’s email).
 
-        Users who want to get NFT airdrops must register on the Matrix website with the same wallet holding listed NFTs before the pre-sale starts. The snapshot will be taken right after the early access closes.
+          Users who want to get NFT airdrops must register on the Matrix website with the same wallet holding listed NFTs before the pre-sale starts. The snapshot will be taken right after the early access closes.
 
-        Thank you for your participation and support! We hope everyone has a great time :D`,
+          Thank you for your participation and support! We hope everyone has a great time :D`,
         },
       },
     },
