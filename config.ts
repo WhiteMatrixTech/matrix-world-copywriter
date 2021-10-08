90c20ee447a444b9261daaf8037c1c1009af80bf
牛叠谊 <18715156450@163.com>
2021-10-08 11:46:50 +0800

    feat: change home page config

--- config.ts
+++ config.ts
@@ -30,8 +30,8 @@ export const copywriter = {
       },
     },
     partnerSection: {
-      title: 'OUR PARTNERS'
-    }
+      title: 'OUR COLLABORATORS',
+    },
   },
   earlyAccess: {
     referralContest: {
