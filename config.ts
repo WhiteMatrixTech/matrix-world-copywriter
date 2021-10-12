6f4f4ece116349d9fe72f7b90ee532f8e3f00ef1
张瑞 <zrnju@qq.com>
2021-10-12 17:51:21 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -156,7 +156,7 @@ export const copywriter = {
       section: {
         section1: {
           title: 'Airdrop for Ethereum Users',
-          content: `We will offer free NFT airdrops to the owner of the below NFT collections who participated in the early access. The owner of NFTs listed below will will be airdropped one NFT for each owned NFT collection.  <span class="hightLight">The snapshot will be taken on Oct 19th 00:00 PDT.</span>
+          content: `We will offer free NFT airdrops to the owner of the below NFT collections who participated in the early access. The owner of NFTs listed below will will be airdropped one NFT for each owned NFT collection. <span class="hightLight">The snapshot will be taken on Oct 19th 00:00 PDT.</span>
 .
 
         Note that multiple NFTs of the same collection only count once. For instance, If Sam has 3 Cryptopunks, 2 Meetbits, and 5 Rivermen, he will get 3 airdropped NFTs.`,
@@ -214,16 +214,12 @@ export const copywriter = {
 
         Users who want to get NFT airdrops must register on the Matrix website with the same wallet holding listed NFTs before the pre-sale starts. The snapshot will be taken right after the early access closes.
 
-        Thank you for your participation and support! We hope everyone has a great time :D
-        
-        Note：
-        
-        Dear Matrixers! Matrix team appreciates your great response to the "NFT AIRDROP for ChainIDE users" activity. Unfortunately, it was discovered that there are widespread abuses of creating a massive number of fraudulent accounts on GitHub to take advantage of the NFT airdrop. The Github team found this issue and notified our partner, the ChainIDE team, about the bad influence of bots-created garbage accounts. 
-        
-        Therefore, after hours of research and discussion, we have decided to close this activity. Members who already registered and created a project before Oct 12th, 01:00 PM PDT will still be rewarded with special NFTs related to Matrix World accessories. 
-
-        Thanks again, and good luck!`,
-        
+        Thank you for your participation and support! We hope everyone has a great time :D`,
+          
+          title: 'Notification of ChainIDE Airdrop Event Termination',
+          content:
+            'Dear Matrixers! Matrix team appreciates your great response to the "NFT AIRDROP for ChainIDE users" activity. Unfortunately, it was discovered that there are widespread abuses of creating a massive number of fraudulent accounts on GitHub to take advantage of the NFT airdrop. The Github team found this issue and notified our partner, the ChainIDE team, about the bad influence of bots-created garbage accounts. Therefore, after hours of research and discussion, we have decided to close this activity. Members who already registered and created a project before Oct 12th, 01:00 PM PDT will still be rewarded with special NFTs related to Matrix World accessories. Thanks again, and good luck!',
+    
         },
       },
     },
