88dc4170cd72b1a8f987455f34c6db5c35c6b555
nemo <18136225579@163.com>
2021-10-22 10:05:08 +0800

    update

--- config.ts
+++ config.ts
@@ -587,7 +587,7 @@ gallery featuring Flow's NBA Topshot moments.`,
     noVoucherContent: `We're currently reviewing your application, thank you for your patience.`,
     purchase: {
       tip: `Your Land voucher(s) to claim`,
-      expire: `expire in`,
+      expire: `expired in`,
       claim: `CLAIM`,
       expireTip: `Your Land voucher(s) have expired.
       For further info, keep an eye on the mailbox and the notification
