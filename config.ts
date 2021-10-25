9445c84e1a48c4bb4825276826524fb4f4a24321
牛叠谊 <18715156450@163.com>
2021-10-25 20:20:23 +0800

    feat: add home page wave seciont config

--- config.ts
+++ config.ts
@@ -143,6 +143,23 @@ export const copywriter = {
         },
       },
     },
+    waveSection: {
+      firstWave: {
+        title: '1st WAVE',
+        subTitle: `<span class="white">2000 Lands on </span>Ethereum`
+      },
+      secondWave: {
+        title: '2nd WAVE',
+        subTitle: `<span class="white">5000 Lands on </span> Flow`,
+        desc: `<div>1. The main drop time on Flow will be at <span class="blueAndBold">08:00 AM, on OCT 26th PDT</span>.<div>
+        <div>2. The <span class="blueAndBold">purchasing price</span>  of each Land is 60 FLOW. Please prepare sufficient funds in advance.</div><div>3. Please <span class="blueAndBold">note that</span> your Land Vouchers in the whitelist are <span class="blueAndBold">not guaranteed</span>, we are applying <span class="blueAndBold">First Come Serve</span> rules for the second wave due to the massive number of qualified applicants on Flow.</div>
+        `
+      },
+      thirdWave: {
+        title: '3rd WAVE',
+        subTitle: `<span class="white">3000 Lands on </span> Ethereum`
+      }
+    }
   },
   earlyAccess: {
     referralContest: {
